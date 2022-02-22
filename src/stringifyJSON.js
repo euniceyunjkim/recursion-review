ec9997be47b41dc3891cfcc1aaa99053b9c282d1
dGarcia-HR <anthonygarcia833@gmail.com>
2022-02-22 12:39:45 -0800

    Add edit for testcases to run. Add edit for all but object types to be stringified

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,33 +5,55 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  var result = '';
+
   // check if object
-  if (typeof obj === 'object' && !Array.isArray(obj) && typeof obj !== null) {
-    return 'Fill me in.';
+  if (typeof obj === 'object' && !Array.isArray(obj) && obj !== null) {
+    console.log(typeof obj, obj);
+    if (Object.keys(obj).length === 0) {
+      return '{}';
+    } else if (Object.keys(obj) > 0) {
+      var temp = [];
+      Object.keys(obj).forEach(function (key) {
+        if (typeof obj[key] !== 'function' && obj[key] !== undefined) {
+          temp.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+        }
+      });
+      return '{' + temp + '}';
+    }
   }
 
   // check if string
   if (typeof obj === 'string') {
-    return 'Fill me in.';
+    return '"' + obj + '"';
   }
 
   // check if number
   if (typeof obj === 'number') {
-    return 'Fill me in.';
+    return String(obj);
   }
 
   // check if array
   if (Array.isArray(obj)) {
-    return 'Fill me in.';
+    if (obj.length === 0) {
+      return '[]';
+    } else {
+      var temp = [];
+      _.each(obj, function(item) {
+        temp.push(stringifyJSON(item));
+      });
+      return '[' + temp + ']';
+    }
   }
   // check if null || empty || undefined
-  if (typeof obj === undefined || typeof obj === null) {
-    return 'Fill me in.';
+  if (typeof obj === undefined || obj === null) {
+    console.log('when undefined or null: ', JSON.stringify(obj));
+    return 'null';
   }
 
   // check if boolean
   if (typeof obj === 'boolean') {
-    return 'Fill me in.';
+    return String(obj);
   }
 
 
