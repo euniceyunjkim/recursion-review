f49845340e4115641925b6718da767b5fe84a3a2
dGarcia-HR <anthonygarcia833@gmail.com>
2022-02-22 12:17:49 -0800

    Added Fill me in. to all data types

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,34 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  // check if object
+  if (typeof obj === 'object' && !Array.isArray(obj) && typeof obj !== null) {
+    return 'Fill me in.';
+  }
+
+  // check if string
+  if (typeof obj === 'string') {
+    return 'Fill me in.';
+  }
+
+  // check if number
+  if (typeof obj === 'number') {
+    return 'Fill me in.';
+  }
+
+  // check if array
+  if (Array.isArray(obj)) {
+    return 'Fill me in.';
+  }
+  // check if null || empty || undefined
+  if (typeof obj === undefined || typeof obj === null) {
+    return 'Fill me in.';
+  }
+
+  // check if boolean
+  if (typeof obj === 'boolean') {
+    return 'Fill me in.';
+  }
+
+
 };
